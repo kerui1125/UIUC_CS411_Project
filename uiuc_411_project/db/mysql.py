8192e0090e5105fa4ff09a896ee74b969355eb0a
Juntao2015 <juntaoy2@illinois.edu>
2022-04-16 23:22:08 -0700

    add secrets file

--- uiuc_411_project/db/mysql.py
+++ uiuc_411_project/db/mysql.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlalchemy as sa
 
-from src.uiuc_411_project.db.load_db_secrets import secretloader
+from uiuc_411_project.db.load_db_secrets import secretloader
 
 
 class MYSQL:
@@ -18,9 +18,10 @@ class MYSQL:
     def query1Panda(self, paperThreshold):
         query = f"SELECT count_table.name FROM (SELECT faculty.name FROM faculty, publication, faculty_publication WHERE faculty.id = faculty_publication.faculty_id and faculty_publication.publication_id = publication.id and publication.num_citations > 10 and publication.year = 2012) as count_table GROUP BY count_table.name HAVING count(count_table.name) > {paperThreshold} ORDER BY trim(count_table.name)"
         pandaQuery = pd.read_sql_query(query, self.connection)
-        df = pd.DataFrame(pandaQuery, columns=['name'])
+        df = pd.DataFrame(pandaQuery, columns=['db'])
         print(df)
 
 
 db = MYSQL()
 db.query1Panda(20)
+
