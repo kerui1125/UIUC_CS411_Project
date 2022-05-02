f3e731a96f0d95bb3a21598a0c7cded76e999564
Juntao2015 <juntaoy2@illinois.edu>
2022-05-01 17:04:50 -0700

    merge with publication widget

--- uiuc_411_project/widgets/keyword_publication_list.py
+++ uiuc_411_project/widgets/keyword_publication_list.py
@@ -1,7 +1,23 @@
 from uiuc_411_project.db.mysql import MYSQL
 
+from dash import dcc, html, Dash, Input, Output, State
 import plotly.graph_objects as go
 import pandas as pd
+
+
+def get_publications_by_keyword_widget(app: Dash) -> list:
+    @app.callback(Output('publication-form', 'figure'),
+                  Input('submit-button-keyword', 'n_clicks'),
+                  State('my-keyword', 'value'))
+    def update_output(n_clicks, keyword):
+        return get_keyword_publication_list(keyword)
+
+    return [
+        html.H2(children='Search paper by keyword'),
+        dcc.Input(id='my-keyword', value='input keyword', type='text'),
+        html.Button(id='submit-button-keyword', n_clicks=0, children='Submit'),
+        dcc.Graph(id='publication-form')
+    ]
 
 
 def get_keyword_publication_list(keyword) -> go.Figure:
